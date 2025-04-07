061c27abaa016cadcd7658ec28d6a7282004c23b
liuya <liuya@wicv.cn>
2025-04-07 22:17:43 +0800

    数据迁移及短链获取api

---
+++ src/views/smart/communityOperating/keywords/aev.vue
@@ -0,0 +1,258 @@
+<template>
+  <div class="flex">
+    <div class="fxauto g-margin-r">
+      <div class="g-card">
+        <div class="g-card-title fxbw">
+          基础信息
+          <el-tag class="cp" v-if="isDetail" size="large" effect="dark" @click="getDetail">同步</el-tag>
+        </div>
+        <el-form
+          :model="form"
+          ref="form"
+          :rules="rules"
+          label-width="100px"
+          class="fxauto g-margin-r"
+          :class="isDetail && 'form-detail'"
+          :disabled="isDetail">
+          <el-form-item label="链接标题" prop="title">
+            <el-input v-model="form.title" maxlength="15" show-word-limit placeholder="请输入" clearable />
+          </el-form-item>
+          <el-form-item label="链接描述" prop="descrition">
+            <el-input
+              type="textarea"
+              v-model="form.descrition"
+              placeholder="请输入"
+              maxlength="50"
+              show-word-limit></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+
+      <div class="g-card">
+        <div class="g-card-title fxbw">
+          关键词
+          <el-button type="primary" v-if="!isDetail" @click="addOrUpdate()">新建关键词</el-button>
+        </div>
+        <DragSortable :data="form.keyWordGroupSubs" @update:data="sort" :disabled="isDetail">
+          <el-table-column label="关键词" align="center" prop="keyword"></el-table-column>
+          <el-table-column align="center" prop="codeName" label="群活码">
+            <template #default="{ row }">
+              <div class="blod">{{ row.codeName || '—' }}</div>
+              <el-image :src="row.groupCodeUrl" class="code-image"></el-image>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" align="center" v-if="!isDetail">
+            <template #default="{ row, $index }">
+              <el-button text @click="addOrUpdate(row)">编辑</el-button>
+              <el-button text @click="remove(row.id, $index)">删除</el-button>
+            </template>
+          </el-table-column>
+        </DragSortable>
+      </div>
+    </div>
+
+    <div class="preview-wrap g-card mt0" style="flex: 1 1 414px">
+      <div class="g-card-title">链接预览</div>
+      <Preview :data="form" />
+    </div>
+  </div>
+
+  <div class="g-footer-sticky" style="z-index: 10" v-if="!isDetail">
+    <el-button type="primary" @click="submit">保存</el-button>
+    <el-button @click="cancel">取消</el-button>
+  </div>
+
+  <!-- 新建关键词弹窗 -->
+  <el-dialog title="关键词" v-model="dialogVisible" width="600" append-to-body>
+    <FormAutoCreateGroup
+      v-loading="loading"
+      ref="FormAutoCreateGroup"
+      v-model:form="formKeywords"
+      append-to-body
+      :rules="rules"
+      :isDetail="isDetail">
+      <template #="{ form: _form }">
+        <el-form-item label="关键词" prop="keyword">
+          <el-input clearable v-model="_form.keyword" placeholder="请输入" maxlength="15" show-word-limit></el-input>
+        </el-form-item>
+        <el-form-item label="群活码名称" prop="codeName">
+          <el-input clearable v-model="_form.codeName" placeholder="请输入" maxlength="15" show-word-limit></el-input>
+        </el-form-item>
+      </template>
+    </FormAutoCreateGroup>
+    <template #footer>
+      <div>
+        <el-button type="primary" @click="submitFormKeywords">确定</el-button>
+        <el-button @click="dialogVisible = false">取消</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+import {
+  getId,
+  getDetail,
+  getDetailList,
+  addOrUpdate,
+  addOrUpdateCancel,
+  addOrUpdateKeyword,
+  removeSub,
+  batchUpdateKeyWordGroupSub,
+} from './api'
+
+export default {
+  components: {
+    Preview: defineAsyncComponent(() => import('./Preview')),
+    DragSortable: defineAsyncComponent(() => import('@/smart/components/DragSortable')),
+    FormAutoCreateGroup: defineAsyncComponent(() => import('@/smart/components/FormAutoCreateGroup.vue')),
+  },
+  data() {
+    return {
+      loading: false, // 遮罩层
+      // 表单参数
+      form: { keyWordGroupSubs: [] },
+      dialogVisible: false,
+      formKeywords: {},
+      rules: Object.freeze({
+        title: [{ required: true, message: '必填项', trigger: 'blur' }],
+        descrition: [{ required: true, message: '必填项', trigger: 'blur' }],
+        keyWordGroupSubs: [{ required: true, message: '必填项', trigger: 'change' }],
+
+        keyword: [{ required: true, message: '必填项', trigger: 'blur' }],
+        codeName: [{ required: true, message: '必填项', trigger: 'blur' }],
+      }),
+    }
+  },
+  computed: {
+    isDetail() {
+      return this.$route.path.endsWith('detail')
+    },
+  },
+  created() {
+    this.form.id ??= this.$route.query.id
+    this.getDetail()
+  },
+  methods: {
+    /** 获取详情 */
+    getDetail() {
+      let id = this.$route.query.id
+      this.$store.loading = true
+      Promise.resolve()
+        .then(() => {
+          if (!id) {
+            return getId().then(({ data }) => {
+              this.form.id = data
+            })
+          }
+          return getDetail(id).then(({ data }) => {
+            this.form = data
+            return this.getDetailList()
+          })
+        })
+        .finally(() => (this.$store.loading = false))
+    },
+    getDetailList() {
+      this.$store.loading = true
+      return getDetailList({ pageNum: 1, pageSize: 1000, keywordGroupId: this.form.id })
+        .then(({ rows }) => {
+          // 回显适配关键词客群数据结构
+          ;(rows ??= []).forEach((element) => {
+            let chatIdList = element.chatIdList?.split(',')
+            element.groups = element.groupCodeName?.split(',')?.map((e, i) => ({
+              chatId: chatIdList[i],
+              groupName: e,
+            }))
+          })
+          this.form.keyWordGroupSubs = rows
+        })
+        .finally(() => (this.$store.loading = false))
+    },
+    addOrUpdate(row = {}) {
+      this.formKeywords = Object.assign({}, row)
+      this.dialogVisible = true
+    },
+    sort(data) {
+      this.$store.loading = true
+      this.form.keyWordGroupSubs = data
+      batchUpdateKeyWordGroupSub(this.form)
+        .then(() => {
+          this.msgSuccess()
+          return this.getDetailList()
+        })
+        .finally(() => {
+          this.$store.loading = false
+        })
+    },
+    // 新增或更新关键词群集合中的关键词群
+    submitFormKeywords() {
+      this.$refs.FormAutoCreateGroup.validate().then(() => {
+        this.loading = true
+        let form = JSON.parse(JSON.stringify(this.formKeywords))
+        form.chatIdList = form.groups?.map((e) => e.chatId)?.join(',')
+        delete form.groups
+        form.keywordGroupId ??= this.form.id
+        addOrUpdateKeyword(form)
+          .then(({ data }) => {
+            if (form.id) {
+              this.form.keyWordGroupSubs.some((e, i, arr) => {
+                return e.id === form.id && (arr[i] = data)
+              })
+            } else {
+              this.form.keyWordGroupSubs.push(data)
+            }
+            this.dialogVisible = false
+          })
+          .finally(() => {
+            this.loading = false
+          })
+      })
+    },
+
+    remove(id, index) {
+      this.$confirm().then(() => {
+        this.$store.loading = true
+        removeSub(id)
+          .then(() => {
+            this.msgSuccess()
+            return this.getDetailList()
+          })
+          .finally(() => {
+            this.$store.loading = false
+          })
+        // this.form.keyWordGroupSubs.splice(index, 1)
+      })
+    },
+    cancel() {
+      addOrUpdateCancel(this.form)
+      this.$router.back()
+    },
+    // 新增或更新关键词群
+    submit() {
+      this.$refs.form.validate().then(() => {
+        this.$store.loading = true
+        let form = JSON.parse(JSON.stringify(this.form))
+        form.keyWordGroupSubs?.forEach((element, index) => {
+          element.chatIdList = element.groups?.map((e) => e.chatId)?.join(',')
+          delete element.groups
+          element.sort = index
+        })
+        addOrUpdate(this.form)
+          .then(() => {
+            this.msgSuccess('更新成功')
+            this.$router.back()
+          })
+          .finally(() => {
+            this.$store.loading = false
+          })
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.code-image {
+  width: 80px;
+}
+</style>
---
+++ src/views/smart/communityOperating/newCustomer/aev.vue
@@ -0,0 +1,313 @@
+<script>
+import { getDetail, add, update } from './api'
+
+export default {
+  components: { SelectGroup: defineAsyncComponent(() => import('@/smart/components/SelectGroup.vue')) },
+  data() {
+    return {
+      selectedUserList: [],
+      newGroupId: '',
+      dialogVisibleSelectUser: false, // 选择员工会话
+      dialogVisibleSelectTag: false, // 选择客户标签会话
+      dialogVisibleSelectGroup: false, // 选择客群弹窗
+      loading: false,
+      form: {
+        codeName: '', // 活码名称
+        emplList: [], // 员工
+        welcomeMsg: '', //加群引导语
+        groupCodeId: undefined, // 群活码ID
+        skipVerify: false, // 无需确认自动加好友
+        // users: [],
+        // tags: [], // 客户标签
+      },
+      // codes: [],
+      // groupQrCode: {},
+      rules: Object.freeze({
+        codeName: [{ required: true, message: '必填项', trigger: 'blur' }],
+        users: [{ required: true, message: '必填项', trigger: 'blur' }],
+        // tags: [{ required: true, message: '必填项', trigger: 'change' }],
+        welcomeMsg: [{ required: true, message: '必填项', trigger: 'blur' }],
+        groups: [{ required: true, message: '必填项', trigger: 'change' }],
+        linkTitle: [{ required: true, message: '必填项', trigger: 'blur' }],
+        linkDesc: [{ required: true, message: '必填项', trigger: 'blur' }],
+        // groupCodeId: [{ required: true, message: '必填项', trigger: 'blur' }],
+      }),
+    }
+  },
+  computed: {
+    isDetail() {
+      return this.$route.path.endsWith('detail')
+    },
+  },
+  watch: {
+    // tags: {
+    //   deep: true,
+    //   handler(tags) {
+    //     this.form.tagList = tags.map((t) => t.tagId)
+    //   },
+    // },
+    // users: {
+    //   deep: true,
+    //   handler(users) {
+    //     this.form.emplList = users.map((u) => u.businessId)
+    //     this.$refs.form.validateField('emplList')
+    //   },
+    // },
+  },
+  created() {
+    this.$route.query.id && this.getDetail(this.$route.query.id)
+  },
+  methods: {
+    /** 获取详情 */
+    getDetail(id) {
+      this.loading = true
+      getDetail(id).then(({ data }) => {
+        this.form = data
+
+        // if (data.groupCodeInfo && data.groupCodeInfo.id) {
+        //   this.codes = [data.groupCodeInfo]
+        //   this.groupQrCode = data.groupCodeInfo
+        //   this.form.groupCodeId = data.groupCodeInfo.id
+        // } else {
+        //   this.codes = []
+        //   this.groupQrCode = {}
+        //   this.form.groupCodeId = ''
+        // }
+
+        this.loading = false
+      })
+    },
+    // 选择二维码确认按钮
+    // submitSelectQrCode(data) {
+    //   this.groupQrCode = data
+    //   this.form.groupCodeId = data.id
+    //   this.$refs.form.validateField('groupCodeId')
+    // },
+    submit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.loading = true
+          ;(this.$route.query.id ? update : add)(this.form)
+            .then(() => {
+              this.msgSuccess()
+              this.loading = false
+              this.$router.back()
+            })
+            .catch(() => {
+              this.loading = false
+            })
+        }
+      })
+    },
+  },
+}
+</script>
+
+<template>
+  <div v-loading="loading">
+    <div class="flex">
+      <el-form
+        :model="form"
+        ref="form"
+        :rules="rules"
+        label-width="100px"
+        scroll-to-error
+        class="fxauto g-margin-r"
+        :class="isDetail && 'form-detail'"
+        :disabled="isDetail">
+        <div class="g-card">
+          <div class="g-card-title fxbw">
+            基础信息
+            <el-tag
+              class="cp"
+              v-if="isDetail"
+              size="large"
+              effect="dark"
+              @click="$router.push({ path: './aev', query: { id: $route.query.id } })">
+              编辑
+            </el-tag>
+          </div>
+          <el-form-item label="活码名称" prop="codeName">
+            <el-input v-model="form.codeName" maxlength="30" show-word-limit placeholder="请输入" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="使用员工" prop="users">
+            <div v-if="!isDetail">
+              <el-button type="primary" @click="dialogVisibleSelectUser = true">选择员工</el-button>
+            </div>
+            <TagEllipsis :list="form.users" limit="10" emptyText></TagEllipsis>
+          </el-form-item>
+
+          <!-- <el-form-item label="选择群活码" prop="groupCodeId">
+            <el-image
+              style="width: 160px; height: 160px"
+              v-if="groupQrCode && groupQrCode.codeUrl"
+              :src="groupQrCode.codeUrl"
+              class="code-image mr10"></el-image>
+            <el-button type="primary" plain icon="el-icon-plus" @click="dialogVisibleSelectQrCode = true">
+              {{ groupQrCode && groupQrCode.codeUrl ? '修改' : '选择' }}
+            </el-button>
+          </el-form-item> -->
+          <el-form-item label="入群标签" prop="tags">
+            <!-- closable -->
+            <template v-if="!isDetail">
+              <el-button type="primary" @click="dialogVisibleSelectTag = true">选择标签</el-button>
+              <div class="g-tip">通过此群活码进群的客户自动打上选中的标签</div>
+            </template>
+            <TagEllipsis :list="form.tags" limit="10" emptyText></TagEllipsis>
+          </el-form-item>
+          <el-form-item label="添加设置" prop="skipVerify">
+            <el-checkbox v-model="form.skipVerify">客户添加时无需经过确认自动成为好友</el-checkbox>
+          </el-form-item>
+        </div>
+
+        <div class="g-card">
+          <div class="g-card-title">拉群设置</div>
+          <el-form-item label="加群引导语" prop="welcomeMsg">
+            <TextareaExtend
+              v-model="form.welcomeMsg"
+              maxlength="220"
+              show-word-limit
+              :autosize="{ minRows: 5, maxRows: 20 }"
+              placeholder="请输入"
+              clearable></TextareaExtend>
+          </el-form-item>
+          <el-form-item label="活码客群:" prop="groups">
+            <template v-if="!isDetail">
+              <el-button type="primary" @click="dialogVisibleSelectGroup = true">选择客群</el-button>
+              <div class="g-tip">最多选择五个客群</div>
+            </template>
+            <TagEllipsis :list="form.groups" limit="5" defaultProps="groupName" emptyText></TagEllipsis>
+          </el-form-item>
+
+          <el-form-item label="群满自动建群:">
+            <el-switch v-model="form.autoCreateRoom" :active-value="1" :inactive-value="0"></el-switch>
+            <div class="g-tip">默认以第一个群的群主作为新建群的群主</div>
+          </el-form-item>
+          <template v-if="form.autoCreateRoom">
+            <el-form-item label="群名前缀:" prop="roomBaseName">
+              <el-input
+                show-word-limit
+                maxlength="20"
+                v-model="form.roomBaseName"
+                placeholder="请输入群名前缀"></el-input>
+            </el-form-item>
+            <el-form-item label="群起始序号:" prop="roomBaseId">
+              <el-input-number v-model="form.roomBaseId" controls-position="right" :min="1"></el-input-number>
+            </el-form-item>
+          </template>
+
+          <el-form-item label="链接标题" prop="linkTitle">
+            <el-input v-model="form.linkTitle" maxlength="20" show-word-limit placeholder="请输入" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="链接描述" prop="linkDesc">
+            <el-input
+              v-model="form.linkDesc"
+              maxlength="30"
+              show-word-limit
+              placeholder="请输入"
+              clearable
+              type="textarea"
+              :autosize="{ minRows: 3, maxRows: 20 }"></el-input>
+          </el-form-item>
+          <el-form-item label="链接封面" prop="linkCoverUrl">
+            <upload v-model:fileUrl="form.linkCoverUrl" type="0" :maxSize="2" :format="['jpg', 'jpeg', 'png']">
+              <template #tip><div>支持jpg/jpeg/png格式，图片大小不超过2M</div></template>
+            </upload>
+          </el-form-item>
+        </div>
+      </el-form>
+      <div>
+        <div class="g-card sticky-t" v-if="isDetail">
+          <div class="g-card-title">活码</div>
+          <div class="ac">
+            <el-image class="" :src="form.emplCodeUrl" fit="contain" style="width: 120px"></el-image>
+            <div class="mt10">
+              <el-button text @click="downloadBlob(form.emplCodeUrl, '新客拉群活码.png', 'image')">下载活码</el-button>
+              <el-button class="copy" text @click="$copyText(form.emplCodeUrl)">复制链接</el-button>
+            </div>
+          </div>
+        </div>
+        <div class="preview-wrap g-card sticky-t">
+          <div class="g-card-title">预览</div>
+          <PhoneDialog
+            :list="[
+              { text: form.welcomeMsg || '请输入加群引导语' },
+              { title: form.linkTitle, desc: form.linkDesc, image: form.linkCoverUrl },
+            ]" />
+        </div>
+      </div>
+    </div>
+
+    <div class="g-footer-sticky" style="z-index: 10" v-if="!isDetail">
+      <el-button type="primary" @click="submit">保存</el-button>
+      <el-button @click="$router.back()">取消</el-button>
+    </div>
+
+    <!-- 选择使用员工弹窗 -->
+    <SelectUser
+      v-model:visible="dialogVisibleSelectUser"
+      title="选择使用员工"
+      :defaultValues="form.users"
+      @success="(data) => ((form.users = data), $refs.form.validateField('users'))"></SelectUser>
+
+    <!-- 选择标签弹窗 -->
+    <SelectTag
+      v-model:visible="dialogVisibleSelectTag"
+      :selected="form.tags"
+      @success="(data) => ((form.tags = data), $refs.form.validateField('tags'))"></SelectTag>
+
+    <!-- 选择客群弹窗 -->
+    <SelectGroup
+      v-model:visible="dialogVisibleSelectGroup"
+      :defaults="form.groups"
+      @submit="(data) => ((form.groups = data), $refs.form.validateField('groups'))"></SelectGroup>
+
+    <!-- 选择二维码弹窗 -->
+    <!-- <SelectQrCode
+      v-model:visible="dialogVisibleSelectQrCode"
+      @success="submitSelectQrCode"
+      :selected="codes"></SelectQrCode> -->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+  margin-top: 24px;
+  .el-form {
+    margin-right: 10%;
+  }
+}
+.preview-wrap {
+  line-height: 26px;
+}
+.welcome-input {
+  display: table;
+  width: 80%;
+  margin: 0 auto 20px;
+}
+.code-image {
+  width: 100px;
+  height: 100px;
+}
+
+.msg-title {
+  font-weight: 500;
+}
+.msg-content {
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+  margin-top: 2px;
+}
+.msg-desc {
+  font-size: 12px;
+  color: var(--font-black-6);
+}
+.phone-dialog-image {
+  width: 50px;
+  height: 50px;
+  border-radius: 6px;
+  border: 1px solid var(--border-black-11);
+}
+</style>
